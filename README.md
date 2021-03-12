f50d508487211cf1836fc937843bf086efafce20
David Savell <dsavell21@gmail.com>
2021-03-12 17:43:19 +0000

    feat(*): Initial Codebase (#2)
    BREAKING CHANGE: Initial Codebase

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # docker-baseimage-minideb
 
-A Docker base image for dsavell.
+[![GitHub Super-Linter](https://github.com/dsavell/docker-baseimage-minideb/workflows/Lint%20Code%20Base/badge.svg)](https://github.com/marketplace/actions/super-linter)
--- build/build.sh
+++
@@ -1,9 +0,0 @@
-#/bin/bash
-
-export DOCKER_ID_USER=''
-
-docker build -t baseimage-minideb .
-docker tag baseimage-minideb ${DOCKER_ID_USER}/baseimage-minideb:latest
-docker tag baseimage-minideb ${DOCKER_ID_USER}/baseimage-minideb:buster
-docker push ${DOCKER_ID_USER}/baseimage-minideb:latest
-docker push ${DOCKER_ID_USER}/baseimage-minideb:buster
